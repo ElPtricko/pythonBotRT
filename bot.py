e48fb08b84f6889a6210232fd2618eddf95dd514
ElPtricko <elptricko@gmail.com>
2019-01-18 13:18:15 +0100

    Modified/Added variables

--- bot.py
+++ bot.py
@@ -1,16 +1,20 @@
+from discord.ext import commands
+from discord.ext.commands import Bot
 import discord
 import asyncio
 
 client = discord.Client()
+server = discord.Server
+user = discord.Member
 
-@client.event
+bot = Bot(command_prefix="?")
+
+@bot.event
 async def on_ready():
     print("Bot is oooon!")
-    await client.change_presence(game=None, status=None, afk=False)
+    await bot.change_presence(game=None, status=None, afk=False)
 
-@client.event 
+@bot.event
 async def on_member_join(member):
     await client.add_roles(member, "member")
     print("Added role member to " + member)
-
-client.run()
