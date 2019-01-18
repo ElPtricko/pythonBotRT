d231cee1cc61c1be9d026be74e52f1ba399680dd
BuildTools <unconfigured@null.spigotmc.org>
2019-01-18 19:20:14 +0100

    nothing

--- bot.py
+++ bot.py
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-from discord.ext import commands
-from discord.ext.commands import Bot
-import discord
-import asyncio
-
-client = discord.Client()
-server = discord.Server
-user = discord.Member
-
-bot = Bot(command_prefix="?")
-
-@bot.event
-async def on_ready():
-    print("Bot is oooon!")
-    await bot.change_presence(game=None, status=None, afk=False)
-
-@bot.event
-async def on_member_join(member):
-    await client.add_roles(member, "member")
-    print("Added role member to " + member)
-=======
 # AutoRole by elptricko#1234
 
 import discord
@@ -44,5 +22,3 @@ async def on_member_join(member):
     print ("Added role Member to user: " + str(member))
 
 bot.run("")
-
->>>>>>> Modfified most of the code.
