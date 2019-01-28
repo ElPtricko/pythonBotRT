9684b689aeeec15509225a431359e7c24f408897
BuildTools <unconfigured@null.spigotmc.org>
2019-01-28 23:30:41 +0100

    Added bulk message delete. It's slow, but works for messages older than 2 weeks.

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@
 
 import discord
 from discord.ext import commands
-from discord.ext.commands import Bot
 import asyncio
 
 bot = commands.Bot(command_prefix="+")
@@ -21,12 +20,21 @@ async def on_member_join(member):
     await bot.add_roles(member, role)
     print ("Added role Member to user: " + str(member))
 
-@bot.command
-async def warn(ctx, user, msg):
-    await ctx.delete_message(ctx.message)
-    await ctx.send("%s, %s" % user % msg)
+@bot.command(pass_context=True)
+async def clear(ctx, amount=100):
+    channel = ctx.message.channel
+    async for msg in bot.logs_from(channel, limit=int(amount+1)):
+        await bot.delete_message(msg)
+    await bot.say("I'm awesome so I deleted %d messages for you, YAY!" % amount)
 
+@bot.command(pass_context=True)
+async def warn(ctx, usr, *msg):
+    channel = ctx.message.channel
+    async for message in bot.logs_from(channel, limit=int(1)):
+        await bot.delete_message(message)
+    stringForYou = ""
+    for word in msg:
+        stringForYou += " " + word    
+    await bot.say("%s, %s" % (usr, stringForYou))
 
-
-bot.add_command(warn)
 bot.run("TOKEN")
